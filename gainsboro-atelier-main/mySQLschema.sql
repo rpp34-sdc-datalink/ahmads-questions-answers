a2d419db6e03fda5d767503a022deb5374b1f0f6
Ahmad Almomani <92074567+ahmadalmomani96@users.noreply.github.com>
2022-05-15 20:26:49 -0700

    Adding Schema's... working mySQL schema

---
+++ gainsboro-atelier-main/mySQLschema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE SDC;
+CREATE DATABASE SDC;
+
+USE SDC;
+
+CREATE TABLE ProductId (
+  Product_Id int NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE Questions (
+  Question_Id int NOT NULL,
+  Question_Body varchar(255) NOT NULL,
+  Question_Date varchar(25) NOT NULL,
+  Asker_Name varchar(40) NOT NULL,
+  Product_Id int,
+  Reported BOOLEAN NOT NULL,
+  PRIMARY KEY (Question_Id),
+  FOREIGN KEY (Product_Id) REFERENCES ProductId(Product_Id)
+);
+
+CREATE TABLE Answers (
+  Answer_Id int NOT NULL,
+  Answer_Body varchar(255) NOT NULL,
+  Answer_Date varchar(25) NOT NULL,
+  Answerer_Name varchar(40) NOT NULL,
+  Question_Id int,
+  Helpfulness int NOT NULL,
+  PRIMARY KEY (Answer_Id),
+  FOREIGN KEY (Question_Id) REFERENCES Questions(Question_Id)
+);
+
+CREATE TABLE Photos (
+  Photo_Id int,
+  Photo_url varchar(125),
+  Answer_Id int,
+  PRIMARY KEY (Photo_Id),
+  FOREIGN KEY (Answer_Id) REFERENCES Answers(Answer_Id)
+);
