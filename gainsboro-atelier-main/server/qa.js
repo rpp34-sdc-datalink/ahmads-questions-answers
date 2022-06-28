83ca4159cbdbd3176de5d9f71ea1ac58fdbf0d8f
Ahmad Almomani <92074567+ahmadalmomani96@users.noreply.github.com>
2022-06-27 19:02:55 -0700

    Change to implement AWC EC2 Instance 4

--- gainsboro-atelier-main/server/qa.js
+++ gainsboro-atelier-main/server/qa.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios");
-const {token} = require("../config.js");
+// const {token} = require("../config.js");
 const bodyParser = require("body-parser");
 var mysql = require('mysql');
 
